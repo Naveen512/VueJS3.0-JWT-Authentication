9d0a8cb2f7f5fbee080bf3c7640e348e43f2a5e5
hchowdary <hchowdary@qualminds.com>
2021-01-31 17:02:18 +0530

    invoke refresh token endpoint on page load from the routing

--- src/appRouter.js
+++ src/appRouter.js
@@ -3,6 +3,7 @@ import Home from "./components/Home.vue";
 import Login from "./components/Login.vue";
 import Dashboard from "./components/Dashboard.vue";
 import store from "./store/index";
+import axios from 'axios';
 
 const routes = [
   { path: "/", component: Home, meta: { requiredAuth: false } },
@@ -15,32 +16,44 @@ export const routeConfig = createRouter({
   routes: routes,
 });
 
-routeConfig.beforeEach((to,from, next) => {
-    console.log(store.getters["auth/getAuthData"].token);
-    if(!store.getters["auth/getAuthData"].token){
-        const access_token = localStorage.getItem("access_token");
-        const refresh_token = localStorage.getItem("refresh_token");
-        if(access_token){
-            const data = {
-                access_token:access_token,
-                refresh_token:refresh_token
-            };
-            store.commit('auth/saveTokenData',data);
-        }
+routeConfig.beforeEach(async (to, from, next) => {
+  console.log(store.getters["auth/getAuthData"].token);
+  if (!store.getters["auth/getAuthData"].token) {
+    const access_token = localStorage.getItem("access_token");
+    const refresh_token = localStorage.getItem("refresh_token");
+    if (access_token) {
+      const data = {
+        access_token: access_token,
+        refresh_token: refresh_token,
+      };
+      store.commit("auth/saveTokenData", data);
     }
-    const auth = store.getters["auth/isTokenActive"];
+  }
+  let auth = store.getters["auth/isTokenActive"];
 
-    if(to.fullPath == "/"){
-        return next();
+  if (!auth) {
+    const authData = store.getters["auth/getAuthData"];
+    if (authData.token) {
+      const payload = {
+        access_token: authData.token,
+        refresh_token: authData.refresh_token,
+      };
+      const refreshResponse = await axios.post(
+        "http://localhost:3000/auth/refreshtoken",
+        payload
+      );
+      store.commit("auth/saveTokenData", refreshResponse.data);
+      auth = true;
     }
-    else if(auth && !to.meta.requiredAuth){
-        return next({path:"/dashboard"});
-    }
-    else if(!auth && to.meta.requiredAuth){
-        return next({path: '/login'});
-    }
+  }
 
+  if (to.fullPath == "/") {
     return next();
+  } else if (auth && !to.meta.requiredAuth) {
+    return next({ path: "/dashboard" });
+  } else if (!auth && to.meta.requiredAuth) {
+    return next({ path: "/login" });
+  }
+
+  return next();
 });
-
-

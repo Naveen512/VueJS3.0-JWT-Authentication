a80ecf7bfd392915c4158dcd0f9ba359591c86d2
hchowdary <hchowdary@qualminds.com>
2021-02-10 18:53:34 +0530

    interceptor issue

--- src/shared/jwtInterceptor.js
+++ src/shared/jwtInterceptor.js
@@ -4,30 +4,41 @@ import store from '../store/index';
 const jwtInterceptor = axios.create({});
 
 
-jwtInterceptor.interceptors.request.use(config => {
-
-    const authData = store.getters['auth/getAuthData'];
-    const isAuthenticated = store.getters['auth/isTokenActive'];
-    if(isAuthenticated){
-        config.headers.common["Authorization"] = `bearer ${authData.token}` ;
-        return config;
+jwtInterceptor.interceptors.request.use((config) => {
+    const authData = store.getters["auth/getAuthData"];
+    if (authData == null) {
+      return config;
     }
-    else{
-        const payload ={
-            access_token: authData.token,
-            refresh_token:authData.refresh_token
+  
+    config.headers.common["Authorization"] = `bearer ${authData.token}`;
+    return config;
+  });
+  
+  jwtInterceptor.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async (error) => {
+      if (error.response.status === 401) {
+        const authData = store.getters["auth/getAuthData"];
+        const payload = {
+          access_token: authData.token,
+          refresh_token: authData.refreshToken,
         };
-        axios.post('http://localhost:3000/auth/refreshtoken',payload)
-        .then(response => {
-            console.log(response);
-            store.commit('auth/saveTokenData',response.data);
-            return jwtInterceptor(config);
-        },error => {
-            config.log(error);
-            return jwtInterceptor(config);
-        });
+  
+        var response = await axios.post(
+          "http://localhost:3000/auth/refreshtoken",
+          payload
+        );
+        await store.dispatch("auth/saveTokensToStorage", response.data);
+        error.config.headers[
+          "Authorization"
+        ] = `bearer ${response.data.access_token}`;
+        return axios(error.config);
+      } else {
+        return Promise.reject(error);
+      }
     }
-
-});
-
-export default jwtInterceptor;
+  );
+  
+  export default jwtInterceptor;
